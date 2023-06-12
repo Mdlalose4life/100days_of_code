5a58491dee77d1a3e7658e76888348b28a16acf9
Mdlalose4life <blessing1059@gmail.com>
2023-06-12 16:59:46 +0200

    MindPal

--- MindPal/app.py
+++ MindPal/app.py
@@ -1,20 +1,75 @@
 #!/usr/bin/python3
 import os
-from flask import Flask, render_template, redirect, flash, url_for
+from flask import Flask, render_template, redirect, flash, url_for, flash
+from flask_wtf import FlaskForm
+from forms import LoginForm, RegisterForm
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from models import db, Conditions, Strategies, User
+from routes import api_db
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+# basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-    'sqlite:///' + os.path.join(basedir, 'mindpal.db')
+# app.config['SQLALCHEMY_DATABASE_URI'] =\
+#    'sqlite:///' + os.path.join(basedir, 'mindpal.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql:///admin:admin123@localhost/mindpal_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+app.config['SECRET_KEY'] = 'secrete secret key'
 migrate = Migrate(app, db)
+app.register_blueprint(api_db)
+login_manager = LoginManager(app)
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+db.init_app(app)
 
-# routes
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@app.route('/logout')
+def logout():
+    if current_user.is_authenticated:
+        logout_user()
+        flash('Logged out successfully..')
+    return redirect(url_for('home'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and check_password_hash(user.password, form.password.data):
+            login_user(user)
+            flash('Logged in successfully')
+            return redirect(url_for('all_condions'))
+        else:
+            flash('invalid password')
+    return render_template('login.html', form=form)
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(
+            form.password.data, method='sha256')
+        new_user = User(name=form.name.data,
+                        surname=form.surname.data,
+                        username=form.username.data,
+                        password=hashed_password,
+                        email=form.email.data)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Registration successfully')
+        return redirect(url_for('login'))
+    return render_template('signup.html', form=form)
 
 
 @app.route('/')
@@ -24,6 +79,42 @@ def index():
 
 @app.route('/home')
 def home():
+    return render_template('home.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/users')
+def get_users():
+    users = User.query.all()
+    user_data = []
+    for user in users:
+        user_info = {
+            'name': user.name,
+            'surname': user.surname,
+            'username': user.username,
+            'email': user.email
+        }
+        user_data.append(user_info)
+    return render_template('users.html', users=user_data)
+
+
+@app.route('/all_condions')
+@login_required
+def all_condions():
     conditions = Conditions.query.all()
     strategies = {}
     for condition in conditions:
@@ -32,135 +123,28 @@ def home():
 
 
 @app.route('/strategy/<int:condition_id>')
+@login_required
 def strategy(condition_id):
     condition = Conditions.query.get(condition_id)
     if condition:
-        strategies = condition.strategy
+        strategies = Strategies.query.filter_by(
+            condition_id=condition.id).all()
         return render_template('strategy.html', condition=condition, strategies=strategies)
     else:
-        return "Condition is not valid"
+        flash('Condition not found.', 'error')
 
 
 @app.route('/strategy_details/<int:strategy_id>')
+@login_required
 def strategy_details(strategy_id):
     strategy = Strategies.query.get(strategy_id)
     if strategy:
         return render_template('strategy_details.html', strategy=strategy)
     else:
         flash('Strtegy not found.', 'error')
-        return redirect(url_for('home'))
 
 
-class User(db.Model):
-    """
-    This is the user Class
-    Attributes
-        email: user email address
-        password: user password
-        first_name: user first name
-        last_name: user last name
-        conditions: choices of conditions for the users
-    """
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    created_on = db.Column(db.DateTime, nullable=True,
-                           default=datetime.utcnow())
-    updated_on = db.Column(db.DateTime, nullable=False,
-                           default=datetime.utcnow())
-    first_name = db.Column(db.String(128), nullable=False)
-    last_name = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), nullable=False)
-    password = db.Column(db.String(128), nullable=False)
-    condition = db.relationship("Conditions", backref="users")
-
-    def __repr__(self):
-        return f'<Post "{self.first_name}"'
-
-
-class Conditions(db.Model):
-    """
-    This class for conditions
-    Attributes:
-    condition_name : name
-    Strategy_id : strategy_id
-    user_id : user_id
-    """
-    __tablename__ = 'conditions'
-    id = db.Column(db.Integer, primary_key=True)
-    created_on = db.Column(db.DateTime, nullable=True,
-                           default=datetime.utcnow())
-    updated_on = db.Column(db.DateTime, nullable=False,
-                           default=datetime.utcnow())
-    condition_name = db.Column(db.String(128), nullable=False)
-    strategy = db.relationship("Strategies", backref="conditions")
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-
-    def __repr__(self):
-        return f'<Post "{self.condition_name}"'
-
-
-class Strategies(db.Model):
-    """
-    This is a Strategy class
-    Attributes:
-        stategy_name: the name of the strategy
-        strategy_text: the text to a strategy
-        youtube_id : The id of a youtube content
-        spotify_id : The id of the spotify content
-    """
-    __tablename__ = 'strategies'
-    id = db.Column(db.Integer, primary_key=True)
-    created_on = db.Column(db.DateTime, nullable=True,
-                           default=datetime.utcnow())
-    updated_on = db.Column(db.DateTime, nullable=False,
-                           default=datetime.utcnow())
-    strategy_name = db.Column(db.String(255), nullable=False)
-    strategy_text = db.Column(db.Text, nullable=False)
-    condition_id = db.Column(db.Integer, db.ForeignKey(
-        'conditions.id'))
-    youtube_content = db.relationship("YoutubeContent", backref="strategies")
-    spotify_content = db.relationship("SpotifyContent", backref="strategies")
-
-
-class SpotifyContent(db.Model):
-    """
-    Spotify Content class
-    Attributes:
-    content_tittle: the title of the content
-    podcast_id: the id of the podcast
-    strategy_id: the id of the strategy to
-    use the content for.
-    """
-    __tablename__ = 'spotifyContent'
-    id = db.Column(db.Integer, primary_key=True)
-    created_on = db.Column(db.DateTime, nullable=True,
-                           default=datetime.utcnow())
-    updated_on = db.Column(db.DateTime, nullable=False,
-                           default=datetime.utcnow())
-    podcast_title = db.Column(db.String(60), nullable=False)
-    podcast_content = db.Column(db.String(200), nullable=False)
-    strategy_id = db.Column(db.String(60), db.ForeignKey(
-        "strategies.id"))
-
-
-class YoutubeContent(db.Model):
-    """
-    The youtube content class  
-    Atribute
-    content_title = Tittle of the youtube content
-    strategy = the strategy to use this content for.
-    """
-    __tablename__ = 'youtubeContent'
-    id = db.Column(db.Integer, primary_key=True)
-    created_on = db.Column(db.DateTime, nullable=True,
-                           default=datetime.utcnow())
-    updated_on = db.Column(db.DateTime, nullable=False,
-                           default=datetime.utcnow())
-    video_title = db.Column(db.String(60), nullable=False)
-    youtube_content = db.Column(db.String(60), nullable=False)
-    strategy_id = db.Column(db.String(60), db.ForeignKey("strategies.id"))
-
-
-app.app_context().push()
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
--- MindPal/init.py
+++ MindPal/init.py
@@ -1,15 +1,16 @@
-from app import db, User, Conditions, Strategies, SpotifyContent, YoutubeContent
+from models import db, User, Conditions, Strategies, SpotifyContent, YoutubeContent, YoutubeContent
+from app import app
 
-user1 = User(first_name='Sbusiso', last_name='Mdlalose',
+user1 = User(name='Sbusiso', surname='Mdlalose', username='sbusiso@camva',
              email='sbusiso@gmail.com', password='sbu123')
-user2 = User(first_name='Ayo', last_name='hisysine',
+user2 = User(name='Ayo', surname='hisysine', username='ayo@camva',
              email='ayo@gmail.com', password='ayo123')
-user3 = User(first_name='Karley', last_name='Moyo',
-             email='Karly@gmail.com', password='karly123')
-user4 = User(first_name='Jack', last_name='Zondo',
-             email='jack@gmail.com', password='jact123')
-user5 = User(first_name='Maswazi', last_name='Nzima',
-             email='maswa@gmail.com', password='karly123')
+user3 = User(name='Karley', surname='Moyo',
+             email='Karly@gmail.com', password='karly123', username='karly@123')
+user4 = User(name='Jack', surname='Zondo',
+             email='jack@gmail.com', password='jact123', username='jack@123')
+user5 = User(name='Maswazi', surname='Nzima',
+             email='maswa@gmail.com', password='karly123', username='mas@123')
 
 
 condition1 = Conditions(condition_name='Anger', users=user1)
@@ -41,18 +42,20 @@ podcast4 = SpotifyContent(
     podcast_title='sit and stand', podcast_content='file/audio050', strategies=strategy4)
 
 youtubeVideo1 = YoutubeContent(
-    video_title='be personal', youtube_content='www.youtube/personal', strategies=strategy1)
+    video_title='be personal', youtube_content='https://www.youtube.com/watch?v=0sOvCWFmrtA', strategies=strategy1)
 youtubeVideo2 = YoutubeContent(
-    video_title='try to understand others', youtube_content='www.youtube/other', strategies=strategy1)
+    video_title='try to understand others', youtube_content='https://www.youtube.com/watch?v=W_72j4UqOSM&list=RDTk6Dbnzpzns&index=5', strategies=strategy1)
 youtubeVideo3 = YoutubeContent(
-    video_title='be personal', youtube_content='www.youtube/personal', strategies=strategy1)
+    video_title='be personal', youtube_content='https://www.youtube.com/watch?v=H6CF_pcO338&list=RDH6CF_pcO338&start_radio=1&rv=W_72j4UqOSM', strategies=strategy1)
 youtubeVideo4 = YoutubeContent(
-    video_title='No more tricky smile', youtube_content='www.youtube/smile', strategies=strategy3)
+    video_title='No more tricky smile', youtube_content='https://www.youtube.com/watch?v=KI7EecQ8-xQ&list=RDH6CF_pcO338&index=9', strategies=strategy3)
 
-db.session.add_all([user1, user2, user3, user4, user5])
-db.session.add_all([condition1, condition2, condition3, condition4])
-db.session.add_all([strategy1, strategy2, strategy3, strategy4])
-db.session.add_all([podcast1, podcast2, podcast3, podcast4])
-db.session.add_all([youtubeVideo1, youtubeVideo2,
-                   youtubeVideo3, youtubeVideo4])
-db.session.commit()
+with app.app_context():
+    db.session.add_all([user1, user2, user3, user4, user5])
+    db.session.add_all([condition1, condition2, condition3, condition4])
+    db.session.add_all([strategy1, strategy2, strategy3, strategy4])
+    db.session.add_all([podcast1, podcast2,
+                       podcast3, podcast4])
+    db.session.add_all([youtubeVideo1, youtubeVideo2,
+                       youtubeVideo3, youtubeVideo4])
+    db.session.commit()
